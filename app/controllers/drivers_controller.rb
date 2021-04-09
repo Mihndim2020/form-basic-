ed0ff10d30f862dc40f254fb8ee07591f358dd46
Mihndim <jelsieprisca2016@gmail.com>
2021-04-09 17:02:58 -0400

    Update controller methods

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,7 +1,18 @@
 class DriversController < ApplicationController
   def index
+    @drivers = Driver.all
   end
 
   def new
+    @driver = Driver.new
   end
+
+  def create 
+    @driver = Driver.new(params.require(:driver).permit(:first_name, :last_name))
+    if @driver.save 
+      redirect_to drivers_url
+    else 
+      render 'new' 
+    end
+  end 
 end
