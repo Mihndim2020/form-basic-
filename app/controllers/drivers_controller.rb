dabc383d72d620bd16396a18522664bc12816d63
Mihndim <jelsieprisca2016@gmail.com>
2021-04-10 10:18:24 -0400

    Add delete path

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -16,6 +16,11 @@ class DriversController < ApplicationController
       render 'new' 
     end
   end 
+  def destroy 
+    @driver = Driver.find(params[:id])
+    @driver.destroy
+    redirect_to drivers_url
+  end
   private 
   def driver_params 
     params.require(:driver).permit(:first_name, :last_name, license_attributes: [:number, :state] )
